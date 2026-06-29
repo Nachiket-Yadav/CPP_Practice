26db9bbf1228b5a2f65226d64b94f3820816bc64
Nachiket-Yadav <nachiyadav@utexas.edu>
2026-06-29 16:48:06 -0500

    LeetCode Maximum Subarray solved

---
+++ LeetCode_MaximumSubarray/maximumsubarray_main.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+class Solution {
+public:
+    int maxSubArray(std::vector<int>& nums) {
+    int maxSoFar = nums[0];
+    int maxEndingHere = nums[0];
+    for (std::size_t i = 1; i < nums.size(); ++i) {
+        const int& x = nums[i];                       // const ref instead of copy
+        maxEndingHere = std::max(x, maxEndingHere + x);
+        maxSoFar = std::max(maxSoFar, maxEndingHere);
+    }
+    return maxSoFar;
+}
+};
+
+int main() {
+    Solution solution;
+    std::vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int result = solution.maxSubArray(nums);
+    return 0;
+}
