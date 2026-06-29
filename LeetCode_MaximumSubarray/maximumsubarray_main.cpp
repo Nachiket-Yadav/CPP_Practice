14d45cee88391063e8ed68bb9a626e2a53fb8006
Nachiket-Yadav <nachiyadav@utexas.edu>
2026-06-29 17:06:57 -0500

    Minor comments and changes to LeetCode Maximum Subarray

--- LeetCode_MaximumSubarray/maximumsubarray_main.cpp
+++ LeetCode_MaximumSubarray/maximumsubarray_main.cpp
@@ -9,7 +9,7 @@ public:
     int maxEndingHere = nums[0];
     for (std::size_t i = 1; i < nums.size(); ++i) {
         const int& x = nums[i];                       // const ref instead of copy
-        maxEndingHere = std::max(x, maxEndingHere + x);
+        maxEndingHere = std::max(x, maxEndingHere + x); // Kadane's algorithm
         maxSoFar = std::max(maxSoFar, maxEndingHere);
     }
     return maxSoFar;
@@ -20,5 +20,6 @@ int main() {
     Solution solution;
     std::vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int result = solution.maxSubArray(nums);
+    std::cout << "Maximum Subarray Sum: " << result << std::endl;
     return 0;
 }
